e1ca583851147cb24418800639837b34e936c434
techieshruti <shrutichandra721@gmail.com>
2025-05-29 14:13:01 +0530

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-base: '/Healthcare_Dashboard/',
+  base: '/Healthcare_Dashboard/',
   plugins: [
     react(),
     tailwindcss()
